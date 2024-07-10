1b2e3a75f5508da49fd145a73ebe7f25a55fc3cc
Justin_Pabs <125973797+JustinPabillore@users.noreply.github.com>
2024-07-10 21:04:47 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { Start, Homepage} from './screens';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Start'
+      >
+        <Stack.Screen
+         name="Start"
+         component={Start}
+         options={{
+         headerShown: false
+         }}
+         />
+        <Stack.Screen
+          name="Homepage"
+          component={Homepage}
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
